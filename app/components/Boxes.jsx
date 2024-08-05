15495f349a6a1abf37fef343d1da353306a7a8f7
merukode <auliarama965@gmail.com>
2024-08-05 13:22:27 +0700

    Update Compoments Box

--- app/components/Boxes.jsx
+++ app/components/Boxes.jsx
@@ -26,9 +26,9 @@ function Boxes() {
         />
       </div>
 
-      <div className="w-full p-8 shadow-5xl flex justify-between rounded-3xl border-2">
+      <div className="w-full bg-[#B9FF66] p-8 shadow-5xl flex justify-between rounded-3xl border-2">
         <section>
-        <p className="text-3xl inline bg-[#B9FF66] bg-">Search Engine Optimization</p>
+        <p className="text-3xl inline bg-[#ffffff] bg-">Pay-per-click advertising</p>
         <span className="flex gap-3 mt-16 items-center">
             <Image
               src="/green-arrow.svg"
@@ -49,12 +49,12 @@ function Boxes() {
         />
       </div>
 
-      <div className="w-full p-8 shadow-5xl flex justify-between rounded-3xl border-2">
+      <div className="w-full bg-[#191A23] p-8 shadow-5xl flex justify-between rounded-3xl border-2">
         <section>
-        <p className="text-3xl inline bg-[#B9FF66] bg-">Search Engine Optimization</p>
+        <p className="text-3xl inline bg-[#ffffff] bg-">Social Media Marketing</p>
         <span className="flex gap-3 mt-16 items-center">
             <Image
-              src="/green-arrow.svg"
+              src="/white-arrow.svg"
               alt="Search"
               width={30}
               height={30}
