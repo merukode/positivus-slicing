eb1db07a14346ed2cecedb4da603b3c3ffb5c004
merukode <auliarama965@gmail.com>
2024-08-05 13:30:01 +0700

    Assets Update

--- app/components/Boxes.jsx
+++ app/components/Boxes.jsx
@@ -118,18 +118,18 @@ function Boxes() {
         />
       </div>
 
-      <div className="w-full p-8 shadow-5xl flex justify-between rounded-3xl border-2">
+      <div className="w-full bg-[#191A23] p-8 shadow-5xl flex justify-between rounded-3xl border-2">
         <section>
-        <p className="text-3xl inline bg-[#B9FF66] bg-">Search Engine Optimization</p>
+        <p className="text-3xl inline bg-[#B9FF66] bg-">Analytics and Tracking</p>
         <span className="flex gap-3 mt-16 items-center">
             <Image
-              src="/green-arrow.svg"
+              src="/white-arrow.svg"
               alt="Search"
               width={30}
               height={30}
               priority
             />
-            <p>Learn More</p>
+            <p className="text-white">Learn More</p>
         </span>
         </section>
         <Image
